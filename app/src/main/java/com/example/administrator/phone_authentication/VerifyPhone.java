014682edcf1bf8b51ea438d7407f39457a48b90a
khushbu63 <modhkhushbu1996@gmail.com>
2018-06-14 09:03:03 +0530

    this is my phone authentication app

---
+++ app/src/main/java/com/example/administrator/phone_authentication/VerifyPhone.java
@@ -0,0 +1,13 @@
+package com.example.administrator.phone_authentication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerifyPhone extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_phone);
+    }
+}
